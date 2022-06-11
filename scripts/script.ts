c603594222c307ecd64e3a9468c5f565cc7dd091
Ivan Maksimov <ivan_kratovo@hotmail.com>
2022-06-11 10:27:55 +0200

    change color added

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,13 +1,13 @@
 class Animals {
-    id : number;
+    id : any;
     name : string;
     gender : string;
     size: string;
     age: number;
     vaccine: string;
     image: string;
-    constructor(id:number, name: string, gender: string, size: string, age:number, vaccine: string, image: string) {
-        this.id = id
+    constructor(id:any, name: string, gender: string, size: string, age:number, vaccine: string, image: string) {
+        this.id = id;
         this.name = name;
         this.gender = gender;
         this.size = size;
@@ -39,7 +39,7 @@ class Cat extends Animals{
     breed: string;
     colorFur: string;
     link: string;
-    constructor(id: number, name: string, gender: string, size: string, age:number, vaccine: string, image: string, breed: string, colorFur:string, link:string) {
+    constructor(id: any, name: string, gender: string, size: string, age:number, vaccine: string, image: string, breed: string, colorFur:string, link:string) {
         super(id, name, gender, size, age, vaccine, image);
         this.breed = breed;
         this.colorFur = colorFur;
@@ -71,7 +71,7 @@ class Cat extends Animals{
 class Dog extends Animals{
     breed:string;
     training:string;
-    constructor(id:number, name: string, gender: string, size: string, age:number, vaccine: string, image: string, breed: string, training:string) {
+    constructor(id:any, name: string, gender: string, size: string, age:number, vaccine: string, image: string, breed: string, training:string) {
         super(id, name, gender, size, age, vaccine, image);
         this.breed = breed;
         this.training = training;
@@ -101,27 +101,29 @@ class Dog extends Animals{
 new Animals(1,"Queen Rasha", "Female", "Small", 2, "Yes", "pic-3.jpg" );
 new Animals(2, "Orlana", "Female", "Big", 5, "Yes", "pic-7.jpg" );
 new Animals(3, "Umka", "Male", "Small", 4, "Yes", "pic-12.jpg" );
-// new Animals("Dank", "Male", "Big", 3, "Yes", "pic-10.jpg" );
-// new Animals("Ninja", "Male", "Small", 12, "Yes", "pic-15.jpg" );
-// new Animals("Felix", "Male", "Medium", 5, "Yes", "pic-9.jpg" );
-// new Animals("Skye", "Female", "Medium", 7, "Yes", "pic-11.jpg" );
-// new Animals("Ruman", "Male", "Medium", 5, "Yes", "pic-17.jpg" );
-// new Animals("Vivi", "Female", "Small", 6, "Yes", "pic-20.jpg" );
-// new Animals("Frigie", "Female", "Big", 2, "Yes", "pic-18.jpg" );
-// new Animals("Quinton", "Male", "Medium", 6, "Yes", "pic-13.jpg" );
-// new Cat("Zap", "Male", "Small", 2, "Yes", "pic-5.jpg", "Maine Cocon", "Tortie", "Dilute Color Fur Gene");
-// new Cat("Zakky", "Male", "Small", 6, "Yes", "pic-6.jpg", "European Shorthair", "Chocolate", "Dilute Color Fur Gene");
-// new Cat("Himalaya", "Female", "Small", 4, "Yes", "pic-8.jpg", "Russian Blue", "Cream", "Dilute Color Fur Gene");
-// new Cat("Desert Rose", "Female", "Small", 2, "Yes", "pic-16.jpg", "American Wirehair", "Snowshoe", "Dilute Color Fur Gene");
-// new Cat("Eulalia", "Female", "Small", 1, "Yes", "pic-19.jpg", "Exotic Shorthair", "Bicolor", "Dilute Color Fur Gene");
-// new Cat("Tyler", "Male", "Small", 5, "Yes", "pic-21.jpg", "Ocicat", "Locket", "Dilute Color Fur Gene");
-// new Dog("Omarion", "Male", "Medium", 5, "Yes", "pic-1.jpg", "Britany", "Yes");
-// new Dog("Berny", "Male", "Small", 4, "Yes", "pic-14.jpg", "Labrador", "Yes");
-// new Dog("Todd", "Male", "Small", 1, "Yes", "pic-22.jpg", "Siberian Husky", "No");
-// new Dog("Nola", "Female", "Small", 2, "Yes", "pic-23.jpg", "English Cocker Spaniel", "No");
-// new Dog("Nessa", "Female", "Small", 5, "Yes", "pic-24.jpg", "Golden Retriver", "Yes");
+new Animals(4, "Dank", "Male", "Big", 3, "Yes", "pic-10.jpg" );
+new Animals(5, "Ninja", "Male", "Small", 12, "Yes", "pic-15.jpg" );
+new Animals(6, "Felix", "Male", "Medium", 5, "Yes", "pic-9.jpg" );
+new Animals(7, "Skye", "Female", "Medium", 7, "Yes", "pic-11.jpg" );
+new Animals(8, "Ruman", "Male", "Medium", 5, "Yes", "pic-17.jpg" );
+new Animals(9, "Vivi", "Female", "Small", 6, "Yes", "pic-20.jpg" );
+new Animals(10, "Frigie", "Female", "Big", 2, "Yes", "pic-18.jpg" );
+new Animals(11, "Quinton", "Male", "Medium", 6, "Yes", "pic-13.jpg" );
+new Cat(12, "Zap", "Male", "Small", 2, "Yes", "pic-5.jpg", "Maine Cocon", "Tortie", "Dilute Color Fur Gene");
+new Cat(13, "Zakky", "Male", "Small", 6, "Yes", "pic-6.jpg", "European Shorthair", "Chocolate", "Dilute Color Fur Gene");
+new Cat(14, "Himalaya", "Female", "Small", 4, "Yes", "pic-8.jpg", "Russian Blue", "Cream", "Dilute Color Fur Gene");
+new Cat(15, "Desert Rose", "Female", "Small", 2, "Yes", "pic-16.jpg", "American Wirehair", "Snowshoe", "Dilute Color Fur Gene");
+new Cat(16, "Eulalia", "Female", "Small", 1, "Yes", "pic-19.jpg", "Exotic Shorthair", "Bicolor", "Dilute Color Fur Gene");
+new Cat(17, "Tyler", "Male", "Small", 5, "Yes", "pic-21.jpg", "Ocicat", "Locket", "Dilute Color Fur Gene");
+new Dog(18, "Omarion", "Male", "Medium", 5, "Yes", "pic-1.jpg", "Britany", "Yes");
+new Dog(19, "Berny", "Male", "Small", 4, "Yes", "pic-14.jpg", "Labrador", "Yes");
+new Dog(20, "Todd", "Male", "Small", 1, "Yes", "pic-22.jpg", "Siberian Husky", "No");
+new Dog(21, "Nola", "Female", "Small", 2, "Yes", "pic-23.jpg", "English Cocker Spaniel", "No");
+new Dog(22, "Nessa", "Female", "Small", 5, "Yes", "pic-24.jpg", "Golden Retriver", "Yes");
 
 let main = (document.getElementById("main") as HTMLElement);
+
+console.log(animalsArray)
 
 
 let mainCardsPrint = () => {
@@ -136,24 +138,25 @@ mainCardsPrint();
 
  
  
-let vaccine = (id:number)=> {
+let vaccine = (id:any)=> {
     // let selected = id;
-    let search = animalsArray.find((x)=> x.id === id);
-    console.log(search)
+    let search:any = animalsArray.find((x)=> x.id === id);
+    console.log(search);
     let message = (document.getElementById(id)as HTMLElement);
-    console.log(message)
+    console.log(message);
+    
 
-    if (search?.vaccine === "Yes") {
-        message.innerHTML=`Vaccine: No`;
+    if (search.vaccine === "Yes") {
+        message.innerHTML=`Vaccine: <i class="bi bi-clipboard2-x"></i>`;
         search.vaccine = "No";
         message.style.backgroundColor = "red";
         console.log(search.vaccine)
     } else  {
-        message.innerHTML=`Vaccine: Yes`;
+        message.innerHTML=`Vaccine: <i class="bi bi-award"></i>`;
         search.vaccine = "Yes";
         message.style.backgroundColor = "green";
     }
-    
+    mainCardsPrint();
     
     }
 

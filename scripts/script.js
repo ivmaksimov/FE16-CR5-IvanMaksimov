5283f8673f0551aec7d3a2c112992f4ed89c75ab
Ivan Maksimov <ivan_kratovo@hotmail.com>
2022-06-11 13:01:48 +0200

    sort added

--- scripts/script.js
+++ scripts/script.js
@@ -79,53 +79,76 @@ class Dog extends Animals {
       </div>`;
     }
 }
-new Animals(1, "Queen Rasha", "Female", "Small", 2, "Yes", "pic-3.jpg");
-new Animals(2, "Orlana", "Female", "Big", 5, "Yes", "pic-7.jpg");
-new Animals(3, "Umka", "Male", "Small", 4, "Yes", "pic-12.jpg");
-new Animals(4, "Dank", "Male", "Big", 3, "Yes", "pic-10.jpg");
-new Animals(5, "Ninja", "Male", "Small", 12, "Yes", "pic-15.jpg");
-new Animals(6, "Felix", "Male", "Medium", 5, "Yes", "pic-9.jpg");
-new Animals(7, "Skye", "Female", "Medium", 7, "Yes", "pic-11.jpg");
-new Animals(8, "Ruman", "Male", "Medium", 5, "Yes", "pic-17.jpg");
-new Animals(9, "Vivi", "Female", "Small", 6, "Yes", "pic-20.jpg");
-new Animals(10, "Frigie", "Female", "Big", 2, "Yes", "pic-18.jpg");
-new Animals(11, "Quinton", "Male", "Medium", 6, "Yes", "pic-13.jpg");
-new Cat(12, "Zap", "Male", "Small", 2, "Yes", "pic-5.jpg", "Maine Cocon", "Tortie", "Dilute Color Fur Gene");
-new Cat(13, "Zakky", "Male", "Small", 6, "Yes", "pic-6.jpg", "European Shorthair", "Chocolate", "Dilute Color Fur Gene");
-new Cat(14, "Himalaya", "Female", "Small", 4, "Yes", "pic-8.jpg", "Russian Blue", "Cream", "Dilute Color Fur Gene");
-new Cat(15, "Desert Rose", "Female", "Small", 2, "Yes", "pic-16.jpg", "American Wirehair", "Snowshoe", "Dilute Color Fur Gene");
-new Cat(16, "Eulalia", "Female", "Small", 1, "Yes", "pic-19.jpg", "Exotic Shorthair", "Bicolor", "Dilute Color Fur Gene");
-new Cat(17, "Tyler", "Male", "Small", 5, "Yes", "pic-21.jpg", "Ocicat", "Locket", "Dilute Color Fur Gene");
-new Dog(18, "Omarion", "Male", "Medium", 5, "Yes", "pic-1.jpg", "Britany", "Yes");
-new Dog(19, "Berny", "Male", "Small", 4, "Yes", "pic-14.jpg", "Labrador", "Yes");
-new Dog(20, "Todd", "Male", "Small", 1, "Yes", "pic-22.jpg", "Siberian Husky", "No");
-new Dog(21, "Nola", "Female", "Small", 2, "Yes", "pic-23.jpg", "English Cocker Spaniel", "No");
-new Dog(22, "Nessa", "Female", "Small", 5, "Yes", "pic-24.jpg", "Golden Retriver", "Yes");
+new Animals(1, "Queen Rasha", "Female", "Small", 2, "<i class='bi bi-award'></i>", "pic-3.jpg");
+new Animals(2, "Orlana", "Female", "Big", 5, "<i class='bi bi-clipboard2-x'></i>", "pic-7.jpg");
+new Animals(3, "Umka", "Male", "Small", 4, "<i class='bi bi-clipboard2-x'></i>", "pic-12.jpg");
+new Animals(4, "Dank", "Male", "Big", 3, "<i class='bi bi-clipboard2-x'></i>", "pic-10.jpg");
+new Animals(5, "Ninja", "Male", "Small", 12, "<i class='bi bi-award'></i>", "pic-15.jpg");
+new Animals(6, "Felix", "Male", "Medium", 5, "<i class='bi bi-clipboard2-x'></i>", "pic-9.jpg");
+new Animals(7, "Skye", "Female", "Medium", 7, "<i class='bi bi-award'></i>", "pic-11.jpg");
+new Animals(8, "Ruman", "Male", "Medium", 5, "<i class='bi bi-clipboard2-x'></i>", "pic-17.jpg");
+new Animals(9, "Vivi", "Female", "Small", 6, "<i class='bi bi-clipboard2-x'></i>", "pic-20.jpg");
+new Animals(10, "Frigie", "Female", "Big", 2, "<i class='bi bi-award'></i>", "pic-18.jpg");
+new Animals(11, "Quinton", "Male", "Medium", 6, "<i class='bi bi-award'></i>", "pic-13.jpg");
+new Cat(12, "Zap", "Male", "Small", 2, "<i class='bi bi-clipboard2-x'></i>", "pic-5.jpg", "Maine Cocon", "Tortie", "https://en.wikipedia.org/wiki/Maine_Coon");
+new Cat(13, "Zakky", "Male", "Small", 6, "<i class='bi bi-award'></i>", "pic-6.jpg", "European Shorthair", "Chocolate", "https://en.wikipedia.org/wiki/European_Shorthair");
+new Cat(14, "Himalaya", "Female", "Small", 4, "<i class='bi bi-clipboard2-x'></i>", "pic-8.jpg", "Russian Blue", "Cream", "https://en.wikipedia.org/wiki/Russian_Blue");
+new Cat(15, "Desert Rose", "Female", "Small", 2, "<i class='bi bi-clipboard2-x'></i>", "pic-16.jpg", "American Wirehair", "Snowshoe", "https://en.wikipedia.org/wiki/American_Wirehair");
+new Cat(16, "Eulalia", "Female", "Small", 1, "<i class='bi bi-award'></i>", "pic-19.jpg", "Exotic Shorthair", "Bicolor", "https://en.wikipedia.org/wiki/Exotic_Shorthair");
+new Cat(17, "Tyler", "Male", "Small", 5, "<i class='bi bi-award'></i>", "pic-21.jpg", "Ocicat", "Locket", "https://en.wikipedia.org/wiki/Ocicat");
+new Dog(18, "Omarion", "Male", "Medium", 5, "<i class='bi bi-clipboard2-x'></i>", "pic-1.jpg", "Britany", "Yes");
+new Dog(19, "Berny", "Male", "Small", 4, "<i class='bi bi-award'></i>", "pic-14.jpg", "Labrador", "Yes");
+new Dog(20, "Todd", "Male", "Small", 1, "<i class='bi bi-clipboard2-x'></i>", "pic-22.jpg", "Siberian Husky", "No");
+new Dog(21, "Nola", "Female", "Small", 2, "<i class='bi bi-award'></i>", "pic-23.jpg", "English Cocker Spaniel", "No");
+new Dog(22, "Nessa", "Female", "Small", 5, "<i class='bi bi-clipboard2-x'></i>", "pic-24.jpg", "Golden Retriver", "Yes");
 let main = document.getElementById("main");
-console.log(animalsArray);
 let mainCardsPrint = () => {
-    animalsArray.forEach((animal) => {
-        main.innerHTML += animal.printCards();
-    });
+    for (let i in animalsArray) {
+        if (animalsArray[i].vaccine === "<i class='bi bi-award'></i>") {
+            main.innerHTML += animalsArray[i].printCards();
+            document.getElementById(animalsArray[i].id).style.backgroundColor = "green";
+        }
+        else {
+            main.innerHTML += animalsArray[i].printCards();
+            document.getElementById(animalsArray[i].id).style.backgroundColor = "red";
+        }
+    }
 };
 mainCardsPrint();
 let btnVaccine = [];
-let vaccine = (id) => {
-    // let selected = id;
+let changeColors = (id) => {
     let search = animalsArray.find((x) => x.id === id);
-    console.log(search);
     let message = document.getElementById(id);
-    console.log(message);
-    if (search.vaccine === "Yes") {
-        message.innerHTML = `Vaccine: <i class="bi bi-clipboard2-x"></i>`;
-        search.vaccine = "No";
-        message.style.backgroundColor = "red";
-        console.log(search.vaccine);
-    }
-    else {
-        message.innerHTML = `Vaccine: <i class="bi bi-award"></i>`;
-        search.vaccine = "Yes";
+    if (search.vaccine === "<i class='bi bi-clipboard2-x'></i>") {
+        message.innerHTML = `Vaccine: <i class='bi bi-award'></i>`;
+        search.vaccine = "<i class='bi bi-award'></i>";
         message.style.backgroundColor = "green";
     }
-    mainCardsPrint();
+    else {
+        message.innerHTML = `Vaccine: <i class='bi bi-clipboard2-x'></i>`;
+        search.vaccine = "<i class='bi bi-clipboard2-x'></i>";
+        message.style.backgroundColor = "red";
+    }
 };
+let vaccine = (id) => {
+    let search = animalsArray.find((x) => x.id === id);
+    console.log(search);
+    changeColors(id);
+    //mainCardsPrint();
+};
+let sort = document.getElementById("sort");
+sort.addEventListener("click", function () {
+    animalsArray.sort(function (a, b) {
+        return parseFloat(a.age) - parseFloat(b.age);
+    });
+    main.innerHTML = "";
+    mainCardsPrint();
+});
+let sort2 = document.getElementById("sort2");
+sort2.addEventListener("click", function () {
+    animalsArray.sort(function (a, b) {
+        return parseFloat(b.age) - parseFloat(a.age);
+    });
+    main.innerHTML = "";
+    mainCardsPrint();
+});
